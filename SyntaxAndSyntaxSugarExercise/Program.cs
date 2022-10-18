0cfa36261960fd67e9cf79e006d0405c77ac0ae0
Drew Gregory <drewgregory@otmail.com>
2022-10-18 14:37:48 -0700

    Syntax & Sugar Exercise completion

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            var answer = 4;
+            var respone = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
         }
     }
 }
